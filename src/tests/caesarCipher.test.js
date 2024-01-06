a4771edab3cb6daff6bb3efa44a534f220456efe
HombreOmbre <marcin.obrebowski@outlook.com>
2024-01-06 21:11:51 +0100

    Change test from 'toBe' to 'toMatch'

--- src/tests/caesarCipher.test.js
+++ src/tests/caesarCipher.test.js
@@ -1,21 +1,21 @@
 import caesarCipher from "../scripts/caesarCipher";
 
 it('Test "Hello World" with key 0', () =>
-    expect(caesarCipher('Hello World!', 0)).toBe('Hello World!')
+    expect(caesarCipher('Hello World!', 0)).toMatch(/Hello World!/)
 );
 
 it('Test "Hello World" with key 1', () =>
-    expect(caesarCipher('Hello World!', 1)).toBe('Ifmmp Xpsme!')
+    expect(caesarCipher('Hello World!', 1)).toMatch(/Ifmmp Xpsme!/)
 );
 
 it('Test "Hello World" with key 2', () =>
-    expect(caesarCipher('Hello World!', 2)).toBe('Jgnnq Yqtnf!')
+    expect(caesarCipher('Hello World!', 2)).toMatch(/Jgnnq Yqtnf!/)
 );
 
 it('Test "Hello World" with key 3', () =>
-    expect(caesarCipher('Hello World!', 3)).toBe('Khoor Zruog!')
+    expect(caesarCipher('Hello World!', 3)).toMatch(/Khoor Zruog!/)
 );
 
 it('Test "Hello World" with key 4', () =>
-    expect(caesarCipher('Hello World!', 4)).toBe('Lipps Asvph!')
+    expect(caesarCipher('Hello World!', 4)).toMatch(/Lipps Asvph!/)
 );
